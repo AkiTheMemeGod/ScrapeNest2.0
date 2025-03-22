8ff985c87d383babface5ad9f35613b8409396d6
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-03-22 11:04:09 +0530

    Add User-Agent header to scrape requests for improved compatibility

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ def hello_world():
 @app.route("/scrape", methods=['GET'])
 def scrape():
     url = request.args.get('url')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
     try:
         if not (url.startswith("http://") or url.startswith("https://")):
             raise ValueError("URL must start with 'http://' or 'https://'")
 
-        response = httpx.get(url)
+        response = httpx.get(url, headers=headers)
         response.raise_for_status()
 
         return Response(response.text, content_type='text/html')
